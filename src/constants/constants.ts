bf611cfb8e8cec06cfb2df8b37be0da1e553dc14
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2023-01-26 09:50:51 +0700

    Update | Course Page

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,5 +1,5 @@
 export namespace Constants {
-  // export const BASE_API_ENDPOINT = "http://localhost:3000";
-  export const BASE_API_ENDPOINT =
-    "https://5575-2403-6200-88a0-f6f7-3188-6a59-9d94-8a53.ap.ngrok.io";
+  export const BASE_API_ENDPOINT = "http://localhost:3000";
+  // export const BASE_API_ENDPOINT =
+  //   "https://5575-2403-6200-88a0-f6f7-3188-6a59-9d94-8a53.ap.ngrok.io";
 }
